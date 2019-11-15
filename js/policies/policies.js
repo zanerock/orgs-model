a1a2ab9aec40477e1a08289dfa2ad9d8eeac5449
Zane Rockenbaugh <zane@liquid-labs.com>
2019-11-15 14:20:39 -0600

    get roles [no ci]

--- js/policies/policies.js
+++ js/policies/policies.js
@@ -1,12 +1,37 @@
+import { Roles } from '../roles'
+
 const Policies = class {
+  #roles
+  #rolesFile
 	sourceFiles
 
 	constructor() {
     this.sourceFiles = []
+    this.rolesFile = 'roles.tsv'
   }
 
   addSourceFile(fileName) {
     this.sourceFiles.push(fileName)
+  }
+
+  setRolesFile(name) {
+    this.rolesFile = name
+  }
+
+  getRoles() {
+    if (this.roles !== undefined) return this.roles
+
+    const rolesFile = this.findFile(this.rolesFile)
+    this.roles = new Roles(rolesFile)
+    return this.roles
+  }
+
+  findFile(baseName) {
+    const re = new RegExp(`/${baseName}$`)
+    const results = this.sourceFiles.filter((f) => f.match(re))
+    if (results.length > 1) throw new Error(`Found multiple files matching '${baseName}'`)
+    else if (results.length === 0) return null
+    else return results[0]
   }
 }
 
--- js/policies/policies.test.js
+++ js/policies/policies.test.js
@@ -8,4 +8,12 @@ describe(`Policies`, () => {
     expect(policies.sourceFiles).toHaveLength(1)
     expect(policies.sourceFiles[0]).toBe(testPath)
   })
+
+  test('can get Roles', () => {
+    const policies = new Policies()
+    policies.setRolesFile('test_data.tsv')
+    policies.addSourceFile('./js/roles/test_data.tsv')
+    const roles = policies.getRoles()
+    expect(roles.data).toHaveLength(1)
+  })
 })
