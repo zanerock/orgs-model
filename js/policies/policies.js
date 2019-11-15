1275abf039953e565c73834f8fab616bb822a3d5
Zane Rockenbaugh <zane@liquid-labs.com>
2019-11-15 15:04:32 -0600

    now with document gen [no ci]

--- js/policies/policies.js
+++ js/policies/policies.js
@@ -1,13 +1,17 @@
 import { Roles } from '../roles'
+import * as fs from 'fs'
 
 const Policies = class {
+  #docDir
   #roles
   #rolesFile
+  #terms
 	sourceFiles
 
 	constructor() {
     this.sourceFiles = []
     this.rolesFile = 'roles.tsv'
+    this.terms = []
   }
 
   addSourceFile(fileName) {
@@ -32,6 +36,31 @@ const Policies = class {
     if (results.length > 1) throw new Error(`Found multiple files matching '${baseName}'`)
     else if (results.length === 0) return null
     else return results[0]
+  }
+
+  addTerm(term, definition) {
+    this.terms.push([term, definition])
+  }
+
+  setDocumentDir(dir) {
+    this.docDir = dir
+  }
+
+  generateDocuments() {
+    if (this.docDir === undefined) throw new Error('No document directory defined.')
+    if (!fs.existsSync(this.docDir)) throw new Error(`Target document dir '${this.docDir}' does not exist.`)
+
+    const roles = this.getRoles()
+
+    roles.data.forEach((i) => {
+      this.addTerm(i['Role Name'], i['Job Description'])
+    })
+
+    let glossaryContent = "# Glossary\n\n<dl>"
+    this.terms.sort((a, b) => a[0].localeCompare(b[0]))
+    this.terms.forEach(([term, def]) => glossaryContent += `  <dt>${term}</dt>\n  <dd>${def}</dd>\n\n`)
+    glossaryContent += "</dl>\n"
+    fs.writeFileSync(`${this.docDir}/Glossary.md`, glossaryContent)
   }
 }
 
