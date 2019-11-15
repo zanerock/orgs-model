c78057c6850c50753a750ad66baf78915bc29db8
Zane Rockenbaugh <zane@liquid-labs.com>
2019-11-15 11:48:38 -0600

    narrowed test; got testable [no ci]

--- js/roles.js
+++ js/roles.js
@@ -1,12 +1,10 @@
 import * as fs from 'fs'
 import TSV from 'tsv'
 
-const rolesFile = `./policy/roles.tsv`
-
 const Roles = class {
 	#data
 
-	constructor(file = rolesFile) {
+	constructor(file) {
 		const contents = fs.readFileSync(file, 'utf8')
 		const lines = contents.split("\n")
 		const filteredLines = lines.filter((line) => !line.match(/^\s*$/))
