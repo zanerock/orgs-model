703cf2f409ea367336b4d4f7337903e4aefd5c88
Zane Rockenbaugh <zane@liquid-labs.com>
2019-11-25 03:54:46 -0600

    hotfix unit tests

--- js/roles/roles.js
+++ js/roles/roles.js
@@ -1,11 +1,20 @@
 import { TsvExt } from '../lib'
 
 const Roles = class extends TsvExt {
+	static headers = ['Name', 'Application', 'Super-role', 'Description', 'Notes']
 	static keys = ['name', 'application', 'superRole', 'description', 'notes']
 
 	constructor(fileName) {
-		super(Roles.keys, fileName)
+		super(Roles.headers, Roles.keys, fileName)
 	}
+
+	notUnique(data, item) {
+		let i
+		return -1 !== (i = data.findIndex((line) => line[0].toLowerCase() === item.name.toLowerCase()))
+			&& `Role with name '${item.name}' already exists at entry ${i}.`
+	}
+
+	matchKey = (line, key) => line[0] === key
 }
 
 export { Roles }
