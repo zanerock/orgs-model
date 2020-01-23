2522a2db196552fc45600cbdb98caee0057aa2dd
Zane Rockenbaugh <zane@liquid-labs.com>
2020-01-23 03:00:59 -0600

    liq-refs-gen is now relative [no ci]

--- bin/liq-refs-gen.sh
+++ bin/liq-refs-gen.sh
@@ -1,86 +1,100 @@
-#!/usr/bin/env bash
+#!/usr/bin/env perl
 
-# bash strict settings
-set -o errexit # exit on errors
-set -o nounset # exit on use of uninitialized variable
-set -o pipefail
+use strict; use warnings;
 
-BASE="$PWD"
-OUTPUT="$1"; shift
-ROOT="$1"; shift
+my $out = shift;
+$out = "$ENV{'PWD'}/${out}";
+my $my_package = shift;
+my $rel_path = shift;
+my @paths = @ARGV;
 
-cd "$ROOT"
+my $refs = { "dirs" => {}, "files" => [] };
 
-perl -e '
-  use strict; use warnings;
-
-  my $out = shift;
-  my @paths = split /\n/, $ARGV[0];
-
-  my $refs = { "dirs" => {}, "files" => [] };
-
-  foreach my $path (@paths) {
-    $path =~ s|^/||;
-    my @bits = split /\//, $path;
-    my $tracker = $refs;
-    foreach my $bit (@bits) {
-      exists $tracker->{"dirs"}->{$bit} or $tracker->{"dirs"}->{$bit} = { "dirs" => {}, "files" => [] };
-      $tracker = $tracker->{"dirs"}->{$bit};
+foreach my $path (@paths) {
+  my $is_my_package = (index($path, $my_package) != -1);
+  my @bits = split /\//, $path;
+  my $tracker = $refs;
+  my $path_pivot = 0;
+  for (@bits) {
+    if (/^@/) {
+      $bits[$path_pivot] =~ s/^@//;
+      last;
     }
+    $path_pivot += 1;
+  }
+  if ($is_my_package) { $path_pivot += 3; }
 
-    my $find_str = `cd "$path"; find . -maxdepth 1 -name "*.md"`;
-    my @files = split /\n/, $find_str;
-    map s|\./||, @files;
-
-    $tracker->{"files"} = \@files;
+  foreach my $bit (@bits[$path_pivot...$#bits]) {
+    if ($bit eq "policy") { next; }
+    exists $tracker->{"dirs"}->{$bit} or $tracker->{"dirs"}->{$bit} = { "dirs" => {}, "files" => [] };
+    $tracker = $tracker->{"dirs"}->{$bit};
   }
 
-  # use Data::Dumper; print Dumper($refs);
+  my $find_str;
+  if ($is_my_package) {
+    my @adjustment;
+    my $match_count = 0;
+    my $index = 0;
 
-  open my $fd, ">", "$out" or die "Could not open ouput: $out";
-
-  sub print_refs {
-    my $refs = shift;
-    my $path = shift;
-    my $depth = scalar @$path;
-
-    foreach my $file (@{$refs->{"files"}}) {
-      my $var_name = uc $file;
-      $var_name =~ s/\.MD$//;
-      $var_name =~ s/[\s_-]+/_/g;
-
-      print $fd " " x ($depth * 2 + 2);
-      print $fd "${var_name}: ".join("/", @$path)."/${file}\n";
+    my @rel_bits = split(/\//, $rel_path);
+    for (@bits[$path_pivot...$#bits]) {
+      if ($index > $#rel_bits) {
+        @adjustment = @bits[($path_pivot + $index)...$#bits];
+        last;
+      }
+      elsif ($_ ne $rel_bits[$index]) {
+        @adjustment = ("..") x (scalar(@rel_bits) - $index);
+        push(@adjustment, @bits[($path_pivot + $index)...$#bits]);
+        last;
+      }
+      else {
+        $index += 1;
+      }
+    }
+    if (!@adjustment) {
+      @adjustment = ("..") x (scalar(@rel_bits) - $index);
     }
 
-    foreach my $dir (keys %{$refs->{"dirs"}}) {
-      print $fd " " x ($depth * 2 + 2);
-      print $fd "${dir}:\n";
+    my $find_path = join('/', @adjustment);
+    if (!$find_path) { $find_path = '.'; }
+    $find_str = `cd 'node_modules/${my_package}/policy/${rel_path}'; find '$find_path' -maxdepth 1 -name '*.md'`;
+  }
+  else {
+    $find_str = `cd "${path}"; find . -maxdepth 1 -name "*.md"`;
+  }
+  my @files = split /\n/, $find_str;
 
-      my @new_path = (@$path, ($dir));
-      print_refs($refs->{"dirs"}->{$dir}, \@new_path);
-    }
+  $tracker->{"files"} = \@files;
+}
+
+# use Data::Dumper; print Dumper($refs);
+
+open my $fd, ">", "$out" or die "Could not open ouput: $out";
+
+sub print_refs {
+  my $refs = shift;
+  my $path = shift;
+  my $depth = scalar @$path;
+
+  foreach my $file (@{$refs->{"files"}}) {
+    my $var_name = uc $file;
+    $var_name =~ s/\.MD$//; #}{ # hack to fix Atom beautifier
+    $var_name =~ s|.*/([^/]+)$|$1|; #}{
+    $var_name =~ s/[\s_-]+/_/g; #}
+
+    print $fd " " x ($depth * 2);
+    # print $fd "${var_name}: ".join("/", @$path)."/${file}\n";
+    print $fd "${var_name}: ${file}\n";
   }
 
-  print_refs($refs, []);
-  close $fd;
-  ' "${BASE}/${OUTPUT}" "$(for i in "$@"; do echo "${i/$ROOT/}"; done)"
-exit
+  foreach my $dir (keys %{$refs->{"dirs"}}) {
+    print $fd " " x ($depth * 2);
+    print $fd "${dir}:\n";
 
-LAST_PATH=''
+    my @new_path = (@$path, ($dir));
+    print_refs($refs->{"dirs"}->{$dir}, \@new_path);
+  }
+}
 
-while (( $# > 0 )); do
-  SEARCH_DIR="$1"; shift
-  RELATIVE_PATH="$(echo "${SEARCH_DIR}" | sed -e 's|[^/]*/[^/]*/[^/]*/[^/]*/||')"
-  CURR_PATH="$(dirname "${RELATIVE_PATH}")"
-  if [[ "${CURR_PATH}" != "${LAST_PATH}" ]];
-
-  TYPE="$(basename "$SEARCH_DIR")"
-  SEARCH_DIR="$(echo "$SEARCH_DIR" | sed 's|/$||')"
-
-  echo "$TYPE:"
-  while read REF_FILE; do
-    REF="$(basename "$REF_FILE")"
-    echo -e "  $REF: foo"
-  done < <(find "$SEARCH_DIR" -name "*.md")
-done
+print_refs($refs, []);
+close $fd;
