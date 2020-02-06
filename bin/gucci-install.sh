bda2e911d52ff806d0af8f2dbf859e4d22aa920c
Zane Rockenbaugh <zane@liquid-labs.com>
2020-02-06 12:32:16 -0600

    moved gucci install to it's own @liquid-labs/gucci-wrapper

--- bin/gucci-install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $1 == '--force' ]] || ! [[ -f ./bin/gucci ]]; then
-  VERSION=1.2.2
-  curl -L https://github.com/noqcks/gucci/releases/download/${VERSION}/gucci-v${VERSION}-darwin-amd64 -o ./bin/gucci
-  chmod a+x ./bin/gucci
-fi
