6d29215bfeab0f12ce9acb7346e25846b4a05bf3
Zane Rockenbaugh <zane@liquid-labs.com>
2020-06-27 09:23:34 -0500

    hotfix: recursive template processing
    templates in templates

--- bin/liq-init-docs.pl
+++ bin/liq-init-docs.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 
 use strict; use warnings;
+use Data::Dumper;
 
 if ($#ARGV == -1) { die "Usage:\nliq-init-docs run # to initialize\nand:\nliq-init-docs <file> <data file>"; }
 
@@ -50,36 +51,56 @@ EOF
 
 my $file = shift;
 my $output = shift;
-open(my $fd, "<", $file) or die "Couldn't open input file: $file ($!)";
 open(my $out, ">", $output) or die "Couldn't open data file: $output ($!)";
 # TODO: we won't necessarily recognize a template spread across multilpe lines...
 
 print $out "(\n";
 
-while (<$fd>) {
-  # TODO: extend the spec with a '+' or similar marker for inclusion, then replace it out of the file and build from
-  # the replaced file. This will avoid overloadin the '-''
-  # it's a hack, but we use the '-' to indicate whether it's an external or (no '-') internal template.
-  /\{\{-\s*template\s+"([^"]+)"/ or next;
-  /#no-dep/ and next;
-  my $template = $1;
-  $template =~ s/^\s+|\s+$//g;
-  # we don't currently support cross-including item lists; only the partner file can do that.
-  next if $template =~ /- items$/;
-  $template .= '.tmpl';
+# build the dependency list
+sub process_for_template_deps {
+  my $file = shift;
+  my $deps_tracker = shift;
 
-  my $template_path;
-  for (split /\n/, `find -L 'node_modules/\@liquid-labs' -maxdepth 1 -name "policy-*"`) {
-    my $candidate = `find -L "$_" -path "*${template}" -not -path "node_modules/*/node_modules/*" -not -path "*/.yalc/*"`;
-    chomp($candidate);
-    ($candidate && $template_path) and die "Ambiguous template: ${template}. Found at '$candidate' and '$template_path'.";
-    $candidate && ($template_path = $candidate);
+  open(my $fd, "<", $file) or die "Couldn't open input file: $file ($!)";
+
+  while (<$fd>) {
+    # TODO: extend the spec with a '+' or similar marker for inclusion, then replace it out of the file and build from
+    # the replaced file. This will avoid overloadin the '-''
+    # it's a hack, but we use the '-' to indicate whether it's an external or (no '-') internal template.
+    /\{\{-\s*template\s+"([^"]+)"/ or next;
+    /#no-dep/ and next;
+    my $template = $1;
+    $template =~ s/^\s+|\s+$//g;
+    # we don't currently support cross-including item lists; only the partner file can do that.
+    next if $template =~ /- items$/;
+    $template .= '.tmpl';
+
+    my $template_path;
+    for (split /\n/, `find -L 'node_modules/\@liquid-labs' -maxdepth 1 -name "policy-*"`) {
+      my $candidate = `find -L "$_" -path "*${template}" -not -path "node_modules/*/node_modules/*" -not -path "*/.yalc/*"`;
+      chomp($candidate);
+      ($candidate && $template_path) and die "Ambiguous template: ${template}. Found at '$candidate' and '$template_path'.";
+      $candidate && ($template_path = $candidate);
+    }
+
+    !$template_path and die "Could not find template: ${template}.";
+    $deps_tracker->{$template_path} = 1;
+
+    # now, process that template
+    process_for_template_deps($template_path, $deps_tracker);
   }
 
-  !$template_path and die "Could not find template: ${template}.";
-  print $out "'$template_path',\n";
+  close $fd;
 }
+
+my $deps_tracker2 = {};
+process_for_template_deps($file, $deps_tracker2);
+my @deps = keys %$deps_tracker2;
+
+if (@deps) {
+  print $out "'".join("',\n  '", @deps)."'";
+}
+
 print $out ");\n";
 
-close $fd;
 close $out;
