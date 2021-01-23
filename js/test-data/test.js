e2fb914b9bd573dc2cc28ef5625e8aeffa9e31e4
Zane Rockenbaugh <zane@liquid-labs.com>
2021-01-22 18:45:01 -0600

    hotfix: add hack test to satisify jest in file to satisfy eslint

--- js/test-data/test.js
+++ js/test-data/test.js
@@ -1,4 +1,7 @@
+/* globals test expect */
 /**
 * This is simply a hack acound the (I say bug) that eslint freaks out if it can't find any JavaScript files in a
 * subdirectory of the source dir.
 */
+
+test('Hack test to satisfy eslint bug.', () => expect(true).toBe(true))
