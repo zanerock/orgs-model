870c342b0d33bb6e8a566b242beabed98bdf737c
Zane Rockenbaugh <zane@liquid-labs.com>
2021-01-26 17:08:33 -0600

    flesh out the condition check test [no ci]

--- js/accounts/test/AccountsAPI.test.js
+++ js/accounts/test/AccountsAPI.test.js
@@ -15,9 +15,22 @@ describe('AccountsAPI', () => {
     test.each`
       desc | condition | expectation
       ${'existential sensitivity'} | ${'SENSITIVITY == EXISTENTIAL'} | ${['networks/acme-co']}
+      ${'greater than none'}|${'SENSITIVITY > NONE'}|${['business/fax-co','networks/acme-co']}
+      ${'parameter tags'}|${'BUSINESS'}|${['business/fax-co']}
+      ${'parameter numbers'}|${'REVIEW_PERIOD == 360'}|${['business/fax-co']}
     `('properly evaluates $desc ($condition)', ({ desc, condition, expectation }) => {
       const accts = acctList.filter((acct) => AccountsAPI.checkCondition(condition, acct))
       expect(accts.map(e => e.name)).toEqual(expectation)
     })
+
+    test('complains of unknown parameters', () =>
+      expect(() => AccountsAPI.checkCondition('BLAH', acctList[0]))
+        .toThrow(/'BLAH' is not defined/)
+    )
+
+    test('complains of complicated condition', () =>
+      expect(() => AccountsAPI.checkCondition('exit()', acctList[0]))
+        .toThrow(/unsafe code/)
+    )
   })
 })
